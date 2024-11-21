2f652c56c879ef08dfeefec956a6a0ae9fc2d783
Pedro Chaves <82094495+peacchaves@users.noreply.github.com>
2024-11-21 08:53:15 -0300

    project up v2

---
+++ src/main/java/com/example/cursoonline/CursoonlineApplication.java
@@ -0,0 +1,13 @@
+package com.example.cursoonline;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CursoonlineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CursoonlineApplication.class, args);
+	}
+
+}
